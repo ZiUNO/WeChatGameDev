d046b0dff9db769583209cfa1cd616155aeff6f3
ZiUNO <331245059@qq.com>
2019-04-02 22:08:24 +0800

    Update
    更新choose页面（需修改）

--- pages/choose/choose.js
+++ pages/choose/choose.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    images:{
+      blue_ori: '/image/circle_blue_ori.png',
+      green_ori: '/image/circle_green_ori.png'
+    },
+    choice: 0
   },
 
   /**
@@ -13,57 +17,34 @@ Page({
    */
   onLoad: function (options) {
     //需更改该处-------------------------------------------------------------------------------更改为选择阵营
+    // wx.redirectTo({
+    //   url: '/pages/index/index',
+    // })
+  },
+  chooseBlue: function(){
+    var that = this
+    that.setData({
+      choice: 1,
+      images: {
+        green_ori: '/image/circle_green_ori.png',
+        blue_ori: '/image/circle_blue.png',
+      }
+    })
+  },
+  chooseGreen: function () {
+    var that = this
+    that.setData({
+      choice: 2,
+      images: {
+        green_ori: '/image/circle_green.png',
+        blue_ori: '/image/circle_blue_ori.png',
+      }
+    })
+  },
+  submit: function(){
+    wx.setStorageSync('userChoice', this.data.choice)
     wx.redirectTo({
       url: '/pages/index/index',
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
